f3345eed85f61f35a55704b7536ae4a034985a0f
Avadh Kishore <avadh.kishore@microsoft.com>
2025-01-22 18:13:23 +0530

    Adjusted model.pkl file path

--- Project/tests/test_model.py
+++ Project/tests/test_model.py
@@ -6,8 +6,11 @@
 
 import numpy as np
 from src.predictions import load_model_and_predict
+from tests.test_train_model import test_train_model
 
 def test_predict():
+
+    test_train_model()
     # Test prediction functionality
     sample_data = np.array([[5.1, 3.5, 1.4, 0.2]])
     prediction = load_model_and_predict(sample_data)
