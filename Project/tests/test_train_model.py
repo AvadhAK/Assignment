4a372e4595cb7220b74868f1b06336f0d146d0d1
Avadh Kishore <avadh.kishore@microsoft.com>
2025-01-22 22:23:45 +0530

    Adjusted model.pkl file path

--- Project/tests/test_train_model.py
+++ Project/tests/test_train_model.py
@@ -11,6 +11,8 @@ def test_train_model():
     # Save the model
     parent_dir = os.path.dirname(os.getcwd())  # Get the parent directory
     model_path = os.path.join(parent_dir+"/Project/", "model.pkl")
+    print(model_path)
+    print("Directories", os.listdir(model_path))
     # Train the model and check if the file is created
     assert os.path.exists(model_path), "Model file was not created"
 
