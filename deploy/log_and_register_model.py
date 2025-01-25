0b50065f5d44863cd1f9b673992316c29c0af4b8
Avadh Kishore <avadh.kishore@microsoft.com>
2025-01-25 23:26:56 +0530

    updated code

--- deploy/log_and_register_model.py
+++ deploy/log_and_register_model.py
@@ -2,22 +2,26 @@ import mlflow
 import os
 from mlflow.tracking import MlflowClient
 
+print("1")
 # Set tracking URI
-mlflow.set_tracking_uri(os.getenv('MLFLOW_TRACKING_URI'))
+# mlflow.set_tracking_uri("http://127.0.0.1/5000")
 
 # Define experiment name
-experiment_name = 'my-experiment1'
+experiment_name = 'my-experiment23'
 mlflow.set_experiment(experiment_name)
 
+print("2")
 # Log model
-with mlflow.start_run(run_name='model-deployment-run1') as run:
+with mlflow.start_run(run_name='model-deployment-run23') as run:
     parent_dir = os.path.dirname(os.getcwd())  # Get the parent directory
     model_path = os.path.join(parent_dir, "Assignment", "Project", "model.pkl")
 
+    print(model_path)
     mlflow.log_artifact(model_path, artifact_path='model_artifacts')
 
+    print("2")
     # Register model
     model_uri = f'runs:/{run.info.run_id}/model_artifacts/model.pkl'
-    mlflow.register_model(model_uri, 'my-registered-model1')
+    mlflow.register_model(model_uri, 'my-registered-modeL23')
 
 print('Model registered successfully!')
